bd788d85642481033fc52ef82562a45b29d771f6
Mats Linander <matslina@gmail.com>
2010-05-30 22:13:07 +0200

    a polyglot in bash, C, Tcl and brainfuck

---
+++ polyglot/c_bash_tcl_bf.c
@@ -0,0 +1,16 @@
+#define      _     >++++++[->++++++++++++<]>.----[--<+++>]<
+#define     ___    -.+++++++..+++.[--->+<]>-----.--[-<++++>
+#define    _____   ]<-.--------.+++.------.--------.-[--->+
+#define   _______  <]>.---[---<+>]
+#include <stdio.h>
+#define   OOOOOOO
+#ifndef   OOOOOOO
+#\
+echo "Hello bash world!"
+#\
+cat > /dev/null << EOF_
+puts "Hello tcl world!"
+exit
+#endif
+main(){printf("Hello C world!\n");}
+OOOOOOO
