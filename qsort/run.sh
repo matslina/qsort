b713929028bebfbe610a6bf750d96126f189d9b7
Mats Linander <matslina@gmail.com>
2011-01-08 01:58:07 +0100

    generate gnuplot scripts

--- qsort/run.sh
+++ qsort/run.sh
@@ -2,17 +2,37 @@
 
 set -e
 
-rm *.o || true
+dist=$1
+test -z "$dist" && dist="rand"
 
-for qsort_c in qsort_*.c; do
-    gcc -O2 -c $qsort_c
+implementations=$(ls -1 qsort_*.c | perl -pe "s/qsort_(.*)\.c/\1/")
+
+for impl in $implementations; do
+    gcc -O2 qsort_$impl.c cmp_count.c -o cmp_$impl
+    last_impl=$impl
 done
 
-for qsort_o in qsort_*.o; do
-    gcc -O2 cmp_count.c $qsort_o -o cmp
-    echo $qsort_o
-    ./cmp
-    echo
+for impl in $implementations; do
+    echo $dist $impl
+    ./cmp_${impl} $dist | tee ${dist}_$impl.dat
+    rm cmp_${impl}
 done
 
-rm cmp
+cat > count_$dist.p <<EOF
+set title "sorting $dist data"
+set xlabel "elements"
+set ylabel "comparisons"
+#set logscale x 2
+set key top left
+set key box
+EOF
+
+echo -n "plot " >> count_$dist.p
+for impl in $implementations; do
+    echo -n "'${dist}_${impl}.dat' title \"$impl\" with linespoints" \
+	>> count_$dist.p
+    test $impl == $last_impl && echo >> count_$dist.p || echo ', \' \
+	>> count_$dist.p
+done
+
+echo "pause mouse" >> count_$dist.p
