d553a82b90eb514a128a4a3959b6f8f8cd66bf53
Mats Linander <matslina@gmail.com>
2013-08-04 22:48:07 -0400

    make cmp_count runner script build graphs

--- run.sh
+++ run.sh
@@ -2,25 +2,32 @@
 
 set -e
 
-dist=$1
-test -z "$dist" && dist="rand"
-
-# list implementations
+# list libc implementations
 implementations=$(ls -1 impls/qsort_*.c |
                   perl -pe "s/impls\/qsort_(.*)\.c/\1/" |
 		  sort)
 
-# compile
+# compile comparison counter
 for impl in $implementations; do
     gcc -g impls/qsort_$impl.c cmp_count.c -o cmp_$impl
 done
 
-# run
-for impl in $implementations; do
-    if [ -e ${dist}_$impl.dat ]; then
-	continue
-    fi
-    echo $dist $impl
-    ./cmp_${impl} $dist > ${dist}_$impl.dat || (echo "fail $impl $dist" && rm ${dist}_$impl.dat)
-    rm cmp_${impl}
+# run comparison counter
+for dist in inc dec rand; do
+    for impl in $implementations; do
+	if [ -e ${dist}_$impl.dat ]; then
+	    continue
+	fi
+	echo $dist $impl
+	./cmp_${impl} $dist > ${dist}_$impl.dat || \
+            (echo "fail $impl $dist" && rm ${dist}_$impl.dat)
+    done
 done
+
+# graph the comparison count data
+# (mangling the data was a paing in bash. hence python.)
+python cmp_count_graphs.py
+for f in *.p; do
+    gnuplot $f
+done
+rm *.p
