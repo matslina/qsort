cb93d7747e99b62bd7b37ef39eb63a8ab682756e
Mats Linander <matslina@gmail.com>
2013-08-04 21:20:52 -0400

    breaking out qsort into separate repo

--- polyglot/c_bash.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define EOF_
-#if 0
-echo "Hello world!"
-cat > /dev/null << EOF_
-#endif
-main(){printf("Hello world!\n");}
-EOF_
--- polyglot/c_tcl.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#if 0
-puts "Hello world!"
-if (0) {
-#endif
-main(){printf("Hello world!\n");}
-#if 0
-}
-#endif
